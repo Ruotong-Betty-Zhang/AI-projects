1d09973118ad53fc50e637ffe0fddf5a23c21494
Ruotong-Betty-Zhang <rzha402@aucklanduni.ac.nz>
2024-12-19 11:55:11 +0800

    test a single img data

--- MNIST_classification_logistic_regression.py
+++ MNIST_classification_logistic_regression.py
@@ -3,9 +3,25 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import numpy as np
+import matplotlib.pyplot as plt
 
 # load the MNIST dataset 数字识别
 mnist = fetch_openml('mnist_784')
+
+# take the first image
+img0 = np.array(mnist.data)[0]
+
+# print the shape of the image and the target
+print("img0's shape", img0.shape)
+print("img0's class is ", mnist.target[0])
+
+# reshape the image
+img0_reshape = img0.reshape(28, 28)
+
+# plot the image
+plt.imshow(img0_reshape, cmap='gray')
+# plt.show()
 
 # data preprocessing
 scaler = StandardScaler()
@@ -26,3 +42,5 @@ y_pred = model.predict(X_test)
 # print the accuracy
 print("Accuracy: %.2f" % accuracy_score(y_test, y_pred))
 
+# print the first image prediction
+print("Predicted img0: ", model.predict([img0]))
